802dbb9bf9cc0aeb2c2f4ef94d3f73ce6ad3dfb8
yxs123321 <53416197+yxs123321@users.noreply.github.com>
2026-04-26 23:34:12 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -127,7 +127,7 @@ UPDATE_VERSION() {
 
 
 #删除官方的默认插件
-rm -rf ../feeds/luci/applications/luci-app-{passwall*,mosdns,dockerman,dae*,bypass*}
+rm -rf ../feeds/luci/applications/luci-app-{dae*,bypass*}
 rm -rf ../feeds/packages/net/{v2ray-geodata,dae*}
 cp -r $GITHUB_WORKSPACE/package/* ./
 #修复daed/Makefile
